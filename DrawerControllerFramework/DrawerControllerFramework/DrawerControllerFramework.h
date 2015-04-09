43c6949ee65b9b06ecc11c25fbd1caebf718cb33
Colin Reisterer <creisterer@projectdecibel.com>
2015-04-09 14:03:18 -0400

    Add Dynamic framework with shared scheme
    For Carthage support

---
+++ DrawerControllerFramework/DrawerControllerFramework/DrawerControllerFramework.h
@@ -0,0 +1,19 @@
+//
+//  DrawerControllerFramework.h
+//  DrawerControllerFramework
+//
+//  Created by Colin Reisterer on 4/9/15.
+//  Copyright (c) 2015 Project Decibel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawerControllerFramework.
+FOUNDATION_EXPORT double DrawerControllerFrameworkVersionNumber;
+
+//! Project version string for DrawerControllerFramework.
+FOUNDATION_EXPORT const unsigned char DrawerControllerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerControllerFramework/PublicHeader.h>
+
+
